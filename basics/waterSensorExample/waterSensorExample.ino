3a7e0472a06b0f46dd3240bde0777d9d3a061ced
Snorre Edwin <Snorre.edwin@gmail.com>
2014-11-23 11:22:28 +0000

    added second plant reading

--- basics/waterSensorExample/waterSensorExample.ino
+++ basics/waterSensorExample/waterSensorExample.ino
@@ -8,6 +8,10 @@ void loop() {
   Serial.print(analogRead(0));
   Serial.print("}");
   Serial.println("");
-  delay(300000);
-
+  delay(150000);
+  Serial.print("{\"watervalue2\":");
+  Serial.print(analogRead(1));
+  Serial.print("}");
+  Serial.println("");
+  delay(150000);
 }
